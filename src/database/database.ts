6fbff6453470c76495156bae9d92fd09825cde22
Praveen <pbpraveen1988@gmail.com>
2020-05-03 13:07:00 +0530

    Will initialize the docker for the dataservice.

--- src/database/database.ts
+++ src/database/database.ts
@@ -11,7 +11,16 @@ export class Connections {
         entities: []
     };
 
+
+    /*
+    * @author : Praveen Kumar
+    * @comment : To connect outside of docker container and connect with local, need to use 
+    *           For Mac  -> docker.for.mac.localhost
+    *           For Wind -> docker.for.windows.localhost
+    * @date: 2020-05-03 13:01:53
+    */  
     public static mysql : TypeOrmModuleOptions = {
+        host: "docker.for.mac.localhost",
         type: 'mysql',
         port: 3306,
         username: 'root',
