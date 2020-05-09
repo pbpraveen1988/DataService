04ba381df5097882b4b5a8a08c234ac071ea1fd7
Praveen <pbpraveen1988@gmail.com>
2020-05-09 18:08:22 +0530

    Will pass database details from ORM Config.json

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export class Connections {
-    public static postgres : TypeOrmModuleOptions = {
+    public static postgres: TypeOrmModuleOptions = {
         type: 'postgres',
         port: 5432,
         username: 'postgres',
@@ -11,6 +11,21 @@ export class Connections {
         entities: []
     };
 
+    /*
+    * @author : Praveen Kumar
+    * @comment : Micorsoft Azure Database connections 
+    * @date: 2020-05-09 13:33:19
+    */
+    public static mssql: TypeOrmModuleOptions = {
+        host: 'nestjs.database.windows.net',
+        type: 'mssql',
+        username: 'pbpraveen1988',
+        password: 'Yourtest0001',
+        database: 'nestjs_microapp',
+        synchronize: true,
+        entities: [],
+        port: 1433
+    };
 
     /*
     * @author : Praveen Kumar
@@ -18,8 +33,8 @@ export class Connections {
     *           For Mac  -> docker.for.mac.localhost
     *           For Wind -> docker.for.windows.localhost
     * @date: 2020-05-03 13:01:53
-    */  
-    public static mysql : TypeOrmModuleOptions = {
+    */
+    public static mysql: TypeOrmModuleOptions = {
         host: "docker.for.mac.localhost",
         type: 'mysql',
         port: 3306,
